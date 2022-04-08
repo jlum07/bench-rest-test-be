a4452734ac1d85fb0e88f8382d10eb8f055d5e07
Justin <35445717+jlum07@users.noreply.github.com>
2022-04-08 02:56:32 -0400

    Test for calculating daily totals

--- test/bench_rest_test_be/core_test.clj
+++ test/bench_rest_test_be/core_test.clj
@@ -2,6 +2,21 @@
   (:require [clojure.test :refer :all]
             [bench-rest-test-be.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def mock-transactions [{:Date "2022-04-1" :Amount "10.99"}
+                        {:Date "2022-04-1" :Amount "0.99"}
+                        {:Date "2022-04-2" :Amount "1.55"}
+                        {:Date "2022-04-2" :Amount "100.45"}
+                        {:Date "2022-04-2" :Amount "-3.00"}
+                        {:Date "2022-04-3" :Amount "-10.00"}
+                        {:Date "2022-04-3" :Amount "-50.00"}
+                        {:Date "2022-04-4" :Amount "5"}
+                        {:Date "2022-04-4" :Amount "-5"}])
+
+(def mock-daily-totals {"2022-04-1" 11.98M
+                        "2022-04-2" 99.00M
+                        "2022-04-3" -60.00M
+                        "2022-04-4" 0M})
+
+(deftest daily-totals-tests
+  (testing "Testing calculations of daily totals"
+    (is (= (calculate-daily-totals mock-transactions) mock-daily-totals))))
