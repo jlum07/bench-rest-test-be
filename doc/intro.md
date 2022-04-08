70c8728b6644921e5ff201e33f83e2f9ee342526
Justin <35445717+jlum07@users.noreply.github.com>
2022-04-08 01:19:54 -0400

    Lein new app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bench-rest-test-be
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bench_rest_test_be/core_test.clj
@@ -0,0 +1,7 @@
+(ns bench-rest-test-be.core-test
+  (:require [clojure.test :refer :all]
+            [bench-rest-test-be.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
