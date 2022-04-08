77790efbacba5aac774ca934f4e963e301440416
Justin <35445717+jlum07@users.noreply.github.com>
2022-04-08 02:05:02 -0400

    Getting paginated transaction data

--- src/bench_rest_test_be/core.clj
+++ src/bench_rest_test_be/core.clj
@@ -1,7 +1,24 @@
 (ns bench-rest-test-be.core
+  (:require [clj-http.client :as http]
+            [cheshire.core :as json])
   (:gen-class))
 
+(def api-url "https://resttest.bench.co/transactions/")
+
+(defn get-paginated-api-transactions []
+  (loop [data []
+         page 1]
+    (let [resp (future (http/get (str api-url page ".json")))
+          {:keys [totalCount transactions] :as body-json} (json/parse-string (:body @resp) true)
+          new-data (concat data transactions)]
+      ;(clojure.pprint/pprint body-json)
+      (if (< (count new-data) totalCount)
+        (recur new-data (inc page))
+        new-data))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [transactions (get-paginated-api-transactions)]
+    (clojure.pprint/pprint transactions)
+    )
+  )
