8f0685d813d001a6bcd4d67b9a00dd9c4eb6e0dd
Justin <35445717+jlum07@users.noreply.github.com>
2022-04-08 14:05:28 -0400

    Updating api calls so that calls after the first happen async

--- src/bench_rest_test_be/core.clj
+++ src/bench_rest_test_be/core.clj
@@ -7,6 +7,7 @@
   (str "https://resttest.bench.co/transactions/" page ".json"))
 
 (defn get-paginated-api-transactions []
+  "Calls paginated api sequentially and a list of all transactions"
   (loop [data []
          page 1]
     (let [resp (future (http/get (api-url page)))
@@ -15,6 +16,21 @@
       (if (< (count new-data) totalCount)
         (recur new-data (inc page))
         new-data))))
+
+(defn get-paginated-api-transactions-async []
+  "Calls first page of api to calculate number of pages to call, then calls the remaining pages asynchronously.
+  Assumes 10 transactions per page max."
+  (let [response-1 (future (http/get (api-url 1)))
+        {:keys [totalCount transactions]} (json/parse-string (:body @response-1) true)
+        pages (Math/ceil (/ totalCount 10))]
+    (if (> pages 1)
+        (let [remaining-pages (range 2 (inc pages))
+              rest-responses (future (map (fn [page] (http/get (api-url page))) remaining-pages))
+              rest-transactions (future (mapcat (fn [resp] (-> (:body resp)
+                                                               (json/parse-string true)
+                                                               :transactions)) @rest-responses))]
+          (concat transactions @rest-transactions))
+        transactions)))
 
 (defn calculate-daily-totals [transactions]
   (reduce (fn [acc {:keys [Date Amount]}]
@@ -26,8 +42,7 @@
 
 (defn -main
   [& args]
-  (let [transactions (get-paginated-api-transactions)
+  (let [transactions (get-paginated-api-transactions-async)
         daily-totals (calculate-daily-totals transactions)]
     (clojure.pprint/pprint daily-totals)
-    )
-  )
+    daily-totals))
